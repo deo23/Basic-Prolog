2702d3dd5c0bee7ffa68c59e81ed35af34ce1cf7
Muhammad Deo Audha Rizki <muhammad.deo.tif421@polban.ac.id>
2023-11-12 08:35:13 +0700

    Add new calculation

--- soal3.pl
+++ soal3.pl
@@ -1,23 +1,17 @@
-% Tariff rules
-tariff(0, 1000, 0.08).
-tariff(1001, 10000, 0.04).
-tariff(10001, 20000, 0.02).
-tariff(20001, _, 0.00).
+% Rules for calculating the cost based on travelled kilometres
+calculate_cost(X, Cost) :-
+    X =< 1000,
+    Cost is X * 0.08.
+calculate_cost(X, Cost) :-
+    X > 1000, X =< 10000,
+    Cost is (1000 * 0.08) + ((X - 1000) * 0.04).
+calculate_cost(X, Cost) :-
+    X > 10000, X =< 20000,
+    Cost is (1000 * 0.08) + (9000 * 0.04) + ((X - 10000) * 0.02).
+calculate_cost(X, Cost) :-
+    X > 20000,
+    Cost is (1000 * 0.08) + (9000 * 0.04) + (10000 * 0.02).
 
-% Calculate cost for a specific range
-calculate_cost(Kms, Cost) :-
-    tariff(Min, Max, Rate),
-    Kms >= Min,
-    (Max = -1 ; (Max \= -1, Kms =< Max)), % Check if Max is -1 or Kms is less than or equal to Max
-    Cost is Kms * Rate.
-
-% Calculate total cost based on tariff rules
-total_cost(Kms, Cost) :-
-    findall(PartialCost, calculate_cost(Kms, PartialCost), Costs),
-    sum_list(Costs, Cost).
-
-% Example usage:
-% total_cost(500, Result).
-% total_cost(5000, Result).
-% total_cost(15000, Result).
-% total_cost(25000, Result).
+% Predikat untuk menghitung biaya perjalanan
+total_cost(X, Cost) :-
+    calculate_cost(X, Cost).
